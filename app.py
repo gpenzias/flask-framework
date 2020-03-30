27032bb6e1fcd0b60727cdfa6208d71bad75ec35
Greg Penzias <gpenzias252@gmail.com>
2020-03-30 17:25:57 -0400

    Updated to bokeh v 2.0.1 containing bugfix. Modified app.py to display bokeh plots using jinja2 to render the plot directly in html, as per bokeh tutorial notebook #10. Also cleaned up app.py code a little, by creating a new function.

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
 import os
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+
+import jinja2
+from bokeh.embed import components
+from bokeh.plotting import figure
 
 app = Flask(__name__)
 
@@ -20,9 +24,6 @@ def index():
     # Request was a POST
     app.vars['name_stock_ticker'] = request.form['name_stock_ticker']
 
-    print(f"Stock ticker name: {app.vars['name_stock_ticker']}")
-    save_stock_ticker_graph_img(app.vars['name_stock_ticker'])
-
     return redirect('/graph')
 
 @app.route('/about')
@@ -31,9 +32,12 @@ def about():
 
 @app.route('/graph', methods=['GET'])
 def graph():
-  full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'graph.png')
-  print(f"full_filename={full_filename}")
-  return render_template("graph.html", user_image=full_filename)
+  # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'graph.png')
+  # print(f"full_filename={full_filename}")
+  # return render_template("graph.html", user_image=full_filename)
+  print(f"Stock ticker name: {app.vars['name_stock_ticker']}")
+  template, script, div = create_stock_ticker_graph(app.vars['name_stock_ticker'])
+  return template.render(script=script, div=div)
 
   # return render_template('graph.html')
 
@@ -41,18 +45,114 @@ def graph():
 ### Stock ticker code
 ################################################################################################################
 
-# Get stock data
-def save_stock_ticker_graph_img(name_stock_ticker):
-  curr_ticker = name_stock_ticker
-  curr_metric = 'close' #open, close, high, low
+def load_stock_data_to_dataframe(name_stock_ticker):
+  name_stock_ticker = name_stock_ticker
 
-  params = {'ticker':curr_ticker,'api_key':'rKssTuYeBY6s29yRGzHA'}
+  params = {'ticker': name_stock_ticker, 'api_key': 'rKssTuYeBY6s29yRGzHA'}
   r = requests.get("https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json", params)
 
   col_names = [x['name'] for x in r.json()['datatable']['columns']]
 
-  df = pd.DataFrame(r.json()['datatable']['data'],columns=col_names)
+  df = pd.DataFrame(r.json()['datatable']['data'], columns=col_names)
+  return df
 
+# Get stock data
+def create_stock_ticker_graph(name_stock_ticker):
+  curr_metric = 'close' #could change to open, high, low, etc.
+
+  df = load_stock_data_to_dataframe(name_stock_ticker)
+
+  # IMPORTANT NOTE!! The version of BokehJS loaded in the template should match
+  # the version of Bokeh installed locally.
+
+  template = jinja2.Template("""
+  <!DOCTYPE html>
+  <html lang="en-US">
+
+  <link
+      href="http://cdn.pydata.org/bokeh/release/bokeh-0.13.0.min.css"
+      rel="stylesheet" type="text/css"
+  >
+  <script 
+      src="http://cdn.pydata.org/bokeh/release/bokeh-2.0.1.min.js"
+  ></script>
+  
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Flask app for 12-day course TDI.">
+        <meta name="author" content="gpenzias">
+
+        <title>Graph of stock price Data</title>
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <style>
+        body {
+          padding-top: 70px;
+        }
+        </style>
+
+
+    </head>
+
+	<body>
+        <!-- navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-1">
+              <span class="sr-only">Toggle navigation</span>
+              </button>
+              <span class="navbar-brand">Ticker Lookup App</span>
+            </div>
+        
+            <div class="collapse navbar-collapse" id="nav-1">
+              <ul class="nav navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="/index">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://github.com/gpenzias/flask-framework">GitHub</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="https://www.thedataincubator.com/12day.html">12-day</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+
+
+      <h1>Hello TDI!</h1>
+
+      <p> Please enjoy this simple plot of stock closing prices </p>
+
+      {{ script }}
+
+      {{ div }}
+
+  </body>
+
+  </html>
+  """)
+
+  # create a new plot (with a title) using figure
+  p = figure(x_axis_type="datetime", plot_width=400, plot_height=400,
+             title=f"Quandl WIKI Prices: {name_stock_ticker}: {curr_metric}")
+
+  # add a line renderer
+  p.line(pd.to_datetime(df['date']), df[curr_metric], line_width=1)
+  p.xaxis.axis_label = 'Date'
+  p.yaxis.axis_label = 'Price (U.S. Dollars)'
+
+  # Get html components of plot
+  script, div = components(p)
+
+  return template, script, div
+  """
+  # Old plot generation code with matplotlib, before bokeh 2.0.1 was released. 
   fig = plt.figure(figsize=(12, 6))
   ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
   l1 = ax.plot(pd.to_datetime(df['date']), df[curr_metric])
@@ -65,6 +165,7 @@ def save_stock_ticker_graph_img(name_stock_ticker):
     print('removing old img')
     os.remove(img_path)
   fig.savefig(img_path)
+  """
 
   print(f"Stock ticker name (2nd occurence): {app.vars['name_stock_ticker']}")
 
